b38af0305caf39723fff54fbeec372eb4468dd1d
cristofer-js19 <cristofer.josuan@gmail.com>
2022-07-23 10:17:02 -0300

    Solution: Mario Exercise

---
+++ mario.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Positive Number: ");
+
+        for (int i = 1; i <= height; i++)
+        {
+            for (int j = height; j >= 1; j--)
+            {
+                printf((i >= j) ? "#" : " ");
+            }
+            printf("\n");
+        }
+
+    }
+    while (!(height >= 1 && height < 9));
+}
